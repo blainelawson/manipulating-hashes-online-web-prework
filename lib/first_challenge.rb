62cc5fa577aea8f533dfa4ffe079e60031b97272
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-14 15:28:26 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -20,7 +20,12 @@ def first_challenge
 
   contacts.each do |person, data|
     data.each do |attribute, value|
-      
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+        end
+        
+        
       binding.pry
     end
   end
