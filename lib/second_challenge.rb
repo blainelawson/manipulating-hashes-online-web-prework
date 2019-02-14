df98d7df4034cfcb396467535f0db41445c05ff6
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-14 16:15:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -6,7 +6,9 @@ def second_challenge
    grains: ["rice", "pasta"]
   }
 
-  groceries.collect {|category, item| item.values}
-  
+  groceries.collect do |category, item| 
+    binding.pry
+    item.values
+  end
 
 end
